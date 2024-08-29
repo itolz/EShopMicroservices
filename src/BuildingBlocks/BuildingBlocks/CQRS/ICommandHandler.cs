46e8661b54db011d4cf5b730698118038ab8c568
Ítalo Farias <italo-vinicius.farias@capgemini.com>
2024-08-29 22:39:39 +0100

    implemented ICommandHandler and IQueryHandler interfaces

---
+++ src/BuildingBlocks/BuildingBlocks/CQRS/ICommandHandler.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+
+namespace BuildingBlocks.CQRS
+{
+    //this one for commands that have no return
+    public interface ICommandHandler<in TCommand>
+        : ICommandHandler<TCommand, Unit>
+        where TCommand : ICommand<Unit>
+    {
+    }
+
+    //this one for commands that have return and return should not be null
+    public interface ICommandHandler<in TCommand, TResponse> 
+        : IRequestHandler<TCommand, TResponse>
+        where TCommand : ICommand<TResponse>
+        where TResponse : notnull
+    {
+    }
+}

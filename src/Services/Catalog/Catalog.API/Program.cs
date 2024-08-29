ebde4f53d196400a2b04950189bc47e0eebd239e
Ítalo Farias <italo-vinicius.farias@capgemini.com>
2024-08-30 00:02:44 +0100

    created a specific appsettings.Docker.json

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -6,6 +6,12 @@ builder.Services.AddMediatR(config =>
     config.RegisterServicesFromAssemblies(typeof(Program).Assembly);
 });
 
+builder.Configuration
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true)
+    //.AddJsonFile("appsettings.Docker.json", optional: true)
+    .AddEnvironmentVariables();
+
 //add the services to container
 
 var app = builder.Build();

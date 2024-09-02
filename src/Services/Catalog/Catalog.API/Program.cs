7fc61d94458711a97dcb25a3fb214d1bc8fd8404
Ítalo Farias <italo-vinicius.farias@capgemini.com>
2024-09-02 21:09:21 +0100

    implemented Global exceptions to catch all untreated exceptions to log and return in a pretty json response

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,5 +1,7 @@
 using BuildingBlocks.Behaviors;
 using Marten;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
 using Weasel.Core;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -34,5 +36,32 @@ var app = builder.Build();
 
 //configure the HTTP request pipeline
 app.MapCarter();
+
+app.UseExceptionHandler(exceptionHandlerApp =>
+{
+    exceptionHandlerApp.Run(async context =>
+    {
+        var exception = context.Features.Get<IExceptionHandlerFeature>()?.Error;
+        if (exception == null)
+        { 
+            return; 
+        }
+
+        var problemDetails = new ProblemDetails
+        {
+            Title = exception.Message,
+            Status = StatusCodes.Status500InternalServerError,
+            Detail = exception.StackTrace
+        };
+
+        var logger = context.RequestServices.GetRequiredService<ILogger<Program>>();
+        logger.LogError(exception, exception.Message);
+
+        context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+        context.Response.ContentType = "application/problem+json";
+
+        await context.Response.WriteAsJsonAsync(problemDetails);
+    });
+});
 
 app.Run();
